59cbd04132301972002167cb921b9a1d9b95f001
jbratcher <jeremybratcher@gmail.com>
2018-05-22 19:44:57 -0400

    added lozad.js, changed images to lazy load

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 // Main.js file for custom JS
 
 console.log("Main.js is connected");
+
+// Instantiate lozad.js
+
+const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+observer.observe();
